2b8f68e22ef4648caf401446c000bd4851457e14
Zuchens <paulina.zak1@gmail.com>
2021-12-04 14:51:09 +0100

    Day 4

---
+++ d4/p1.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Row:
+    sum: int
+    marked: int
+@dataclass
+class Board:
+    board_data: List[List[int]]
+    horizontal: List[Row]
+    vertical: List[Row]
+
+def bingo(draw, boards):
+    for draw_element in draw:
+        for idx, board in enumerate(boards):
+           for i in range(len(board.board_data)):
+               for j in range(len(board.board_data[i])):
+                   board_element = board.board_data[i][j]
+                   if draw_element == board_element:
+                        print(draw_element,idx, i, j)
+                        board.horizontal[i].sum -= board_element
+                        board.horizontal[i].marked+=1
+                        board.vertical[j].sum -=board_element
+                        board.vertical[j].marked += 1
+                        if board.vertical[j].marked == 5 or board.horizontal[i].marked == 5:
+                            return board, draw_element
+
+
+
+def process(data):
+    boards = []
+    board_data = []
+    horizontal = []
+    vertical = []
+    for i in range(len(data)-2):
+        row = data[i+2].strip()
+        if row == "":
+            boards.append(Board(board_data, horizontal,vertical))
+            board_data = []
+            horizontal = []
+            vertical = []
+        else:
+            parsed_row = []
+            for element in row.split(" "):
+                if element.strip() =="":
+                    continue
+                parsed_row.append(int(element))
+            board_data.append(parsed_row)
+            horizontal.append(Row(0,0))
+            vertical.append(Row(0,0))
+    boards.append(Board(board_data, horizontal,vertical))
+    return boards
+
+def group(boards):
+    for board in boards:
+        for i in range(len(board.board_data)):
+            for j in range(len(board.board_data[i])):
+                board.horizontal[i].sum+=board.board_data[i][j]
+
+        for i in range(len(board.board_data)):
+            for j in range(len(board.board_data[i])):
+                board.vertical[i].sum+=board.board_data[j][i]
+
+
+with open("input1.txt") as f:
+    data = [ x.strip() for x in f.readlines()]
+draw = [int(x.strip()) for x in data[0].split(",")]
+
+boards = process(data)
+group(boards)
+board, draw_element = bingo(draw, boards)
+unmarked = sum([x.sum for x in board.horizontal])
+print(unmarked,draw_element, unmarked*draw_element)
