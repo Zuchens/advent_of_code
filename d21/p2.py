530a213ddc7402e99dec06862972fc2ce52645de
Zuchens <paulina.zak1@gmail.com>
2021-12-25 17:33:22 +0100

    Day 21 - finish

--- d21/p2.py
+++ d21/p2.py
@@ -1,135 +1,67 @@
 import re
-from copy import copy
-from dataclasses import dataclass
+from collections import defaultdict
 
-from tqdm import tqdm
-# 27075190051476
-# 27075190051476
-# 444356092776315
-# 2140974489582
-# 57806311218714
-with open("input1_example.txt") as f:
+with open("input1.txt") as f:
     data = f.read()
     match1_raw = re.findall("Player 1 starting position: (\d)", data)
 
     match2_raw = re.findall("Player 2 starting position: (\d)", data)
 
-N = 4
-# values = [0 for i in range(30)]
+N = 21
 default_position1 = int(match1_raw[0])
 default_position2 = int(match2_raw[0])
 possibilities  = [0,0,0,1,3,6,7,6,3,1]
-# possibilities  = [2]
-#
-def get_rolls(available_positions, data, worlds, data2):
-    won =0
+
+def get_rolls(available_positions, worlds, won1, won2):
+    print(f"Worlds: {worlds}")
     not_won = 0
-    won2 = 0
-    new_positions = set()
-    new_data = [[0 for positions in range(11)] for points in range(N)]
-    all_possibilities = 0
-    while available_positions:
-        default_position, points, current_possibilities = available_positions.pop()
-        if data[points][default_position] !=0:
-            current_possibilities = data[points][default_position]
-        for roll, roll_possibilities in enumerate(possibilities):
-            all_possibilities+=roll_possibilities
-            if roll_possibilities == 0:
+    new_positions = defaultdict(int)
+    for key, current_possibilities in available_positions.items():
+        player1, player2 = key
+        default_position1, points1 = player1
+        for roll1, roll_possibilities1 in enumerate(possibilities):
+            if roll_possibilities1 == 0:
                 continue
-            if (default_position + roll) % 10 == 0:
-                position = 10
+            if (default_position1 + roll1) % 10 == 0:
+                position1 = 10
             else:
-                position = (default_position + roll) % 10
-            if points + position >= N:
-                won += current_possibilities * roll_possibilities*worlds
-
+                position1 = (default_position1 + roll1) % 10
+            if points1 + position1 >= N:
+                won1 += current_possibilities * roll_possibilities1
             else:
-                new_data[points + position][position] += current_possibilities * roll_possibilities
-                not_won += current_possibilities * roll_possibilities *worlds
-                new_positions.add((position, points + position, new_data[points + position][position]))
-
-    for i in range(len(new_data)):
-        for j in range(len(new_data[i])):
-            new_data[i][j] = new_data[i][j] * worlds
-
-    for i in range(len(data2)):
-        for j in range(len(data2[i])):
-            data2[i][j] = data2[i][j] * not_won
-
-
-            # if won > 0 and data2[i][j] > 0:
-            #     data2[i][j] = data2[i][j] - won
+                default_position2, points2 = player2
+                for roll2, roll_possibilities2 in enumerate(possibilities):
+                    if roll_possibilities2 == 0:
+                        continue
+                    if (default_position2 + roll2) % 10 == 0:
+                        position2 = 10
+                    else:
+                        position2 = (default_position2 + roll2) % 10
+                    if points2 + position2 >= N:
+                        won2 += current_possibilities * roll_possibilities2 * roll_possibilities1
+                    else:
+                        not_won +=current_possibilities * roll_possibilities1*roll_possibilities2
+                        new_positions[(position1, points1 + position1), (position2, points2 + position2)] += current_possibilities * roll_possibilities1*roll_possibilities2
 
 
-    print(not_won, won)
-    a = 1
-    for i in new_data:
-        for j in i:
-            print(f"{j:8d}", end="")
-        print()
-    print()
 
-    for i in data2:
-        for j in i:
-            print(f"{j:8d}", end="")
-        print()
-    print()
+    return new_positions, won1, won2, not_won
 
-    if sum([i for x in data2 for i in x]) !=0:
-        d = sum([i for x in new_data for i in x])
-        e = sum([i for x in data2 for i in x])
-        print(d,e)
-        assert d ==e
-    # for i in range(len(new_data)):
-    #     for j in range(len(new_data[i])):
-    #         new_data[i][j]=new_data[i][j]*worlds
-
-    return new_positions,new_data, won, not_won
-
-all_won = 0
-all_won2 = 0
+won1 = 0
+won2 =0
 worlds = 1
-available_positions = {(default_position1, 0, 1)}
-data = [[0 for positions in range(11)] for points in range(N)]
-available_positions2 = {(default_position2, 0, 1)}
-data2 = [[0 for positions in range(11)] for points in range(N)]
+available_positions = {((default_position1, 0),(default_position2, 0)):1}
+
 prev_won = 0
 while True:
 
-    available_positions, data, won, worlds = get_rolls(available_positions, data, worlds, data2)
-    all_won+=won
+    available_positions, won1, won2,worlds = get_rolls(available_positions, worlds, won1, won2)
+    print(f"Current roll won1 {won1}, won2: {won2} not won {worlds}")
     if len(available_positions)==0:
         break
-    # print(won, worlds)
-    available_positions2, data2, won2, worlds = get_rolls(available_positions2, data2, worlds, data)
-    all_won2 += won2
-    # print(won2, worlds)
-    if len(available_positions2) == 0:
-        break
-    # print("u1")
-    # for i in data:
-    #     for j in i:
-    #         print(f"{j:8d}", end="")
-    #     print()
-    # print()
-    #
-    # print("u2")
-    # for i in data2:
-    #     for j in i:
-    #         print(f"{j:8d}", end="")
-    #     print()
-    # print()
 
-if all_won > all_won2:
-    print(all_won)
+
+if won1 > won2:
+    print(won1)
 else:
-    print(all_won2)
-
-
-print(all_won)
-print(all_won2)
-
-
-
-
-
+    print(won2)
